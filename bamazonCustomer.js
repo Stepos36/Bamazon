31a88967b1751bf87874e21c3bb42d7e726675fa
Stepan <stepos36@gmail.com>
2019-02-09 10:59:05 -0500

    products table displays through CLI-table npm

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -10,7 +11,16 @@ var connection = mysql.createConnection({
 });
 
 connection.query("SELECT * FROM bamazon.products", function(err, res) {
+  var table = new Table({
+    head: ['id', 'product', 'department', 'price', 'quantity']
+  , colWidths: [4, 50, 14, 8, 8]
+  });
+  for(i=0;i<res.length;i++){
+    table.push(
+      [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+    );
+  }
     if (err) throw err;
-    console.log(res);
+    console.log(table.toString());
     connection.end();
   });
